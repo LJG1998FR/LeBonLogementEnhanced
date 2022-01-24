38c52b856d0ddfa2eaf95c9074f311f93092c7ca
LJG1998FR <74655525+LJG1998FR@users.noreply.github.com>
2022-01-24 15:22:51 +0100

    imagesaccueil

--- src/Controller/ImageController.php
+++ src/Controller/ImageController.php
@@ -14,58 +14,11 @@ use Symfony\Component\Routing\Annotation\Route;
 #[Route('/image')]
 class ImageController extends AbstractController
 {
-    #[Route('/', name: 'image_index', methods: ['GET'])]
-    public function index(ImageRepository $imageRepository): Response
-    {
-        return $this->render('image/index.html.twig', [
-            'images' => $imageRepository->findAll(),
-        ]);
-    }
-
-    #[Route('/new', name: 'image_new', methods: ['GET','POST'])]
-    public function new(Request $request): Response
-    {
-        $image = new Image();
-        $form = $this->createForm(ImageType::class, $image);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $entityManager = $this->getDoctrine()->getManager();
-            $entityManager->persist($image);
-            $entityManager->flush();
-
-            return $this->redirectToRoute('image_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('image/new.html.twig', [
-            'image' => $image,
-            'form' => $form,
-        ]);
-    }
-
     #[Route('/{id}', name: 'image_show', methods: ['GET'])]
     public function show(Image $image): Response
     {
         return $this->render('image/show.html.twig', [
             'image' => $image,
-        ]);
-    }
-
-    #[Route('/{id}/edit', name: 'image_edit', methods: ['GET','POST'])]
-    public function edit(Request $request, Image $image): Response
-    {
-        $form = $this->createForm(ImageType::class, $image);
-        $form->handleRequest($request);
-
-        if ($form->isSubmitted() && $form->isValid()) {
-            $this->getDoctrine()->getManager()->flush();
-
-            return $this->redirectToRoute('image_index', [], Response::HTTP_SEE_OTHER);
-        }
-
-        return $this->renderForm('image/edit.html.twig', [
-            'image' => $image,
-            'form' => $form,
         ]);
     }
 
@@ -80,6 +33,10 @@ class ImageController extends AbstractController
             $entityManager = $this->getDoctrine()->getManager();
             $entityManager->remove($image);
             $entityManager->flush();
+            $this->get('session')->getFlashBag()->add(
+                'bien',
+                'Image supprimée avec succès!'
+            );
         }
 
         return $this->redirectToRoute('bien_index', [], Response::HTTP_SEE_OTHER);
