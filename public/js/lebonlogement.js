36044dc64fa9462dbb2c1c4dbb84019671fbfffb
LJG1998FR <74655525+LJG1998FR@users.noreply.github.com>
2022-01-28 12:22:58 +0100

    bdd mysql à jour

--- public/js/lebonlogement.js
+++ public/js/lebonlogement.js
@@ -1,34 +1,16 @@
-window.onload = () => {
-    // Gestion des boutons "Supprimer"
-    let links = document.querySelectorAll("[data-delete]")
-    
-    // On boucle sur links
-    for(link of links){
-        // On écoute le clic
-        link.addEventListener("click", function(e){
-            // On empêche la navigation
-            e.preventDefault()
-
-            // On demande confirmation
-            if(confirm("Voulez-vous supprimer cette image ?")){
-                // On envoie une requête Ajax vers le href du lien avec la méthode DELETE
-                fetch(this.getAttribute("href"), {
-                    method: "DELETE",
-                    headers: {
-                        "X-Requested-With": "XMLHttpRequest",
-                        "Content-Type": "application/json"
-                    },
-                    body: JSON.stringify({"_token": this.dataset.token})
-                }).then(
-                    // On récupère la réponse en JSON
-                    response => response.json()
-                ).then(data => {
-                    if(data.success)
-                        this.parentElement.remove()
-                    else
-                        alert(data.error)
-                }).catch(e => alert(e))
-            }
-        })
+var imagesbien = document.querySelectorAll('.imagesbien');
+var modal = document.getElementById("myModal");
+var modalImg = document.getElementById("img01");
+var captionText = document.getElementById("caption");
+imagesbien.forEach(img => {
+    img.onclick = function(){
+        modal.style.display = "block";
+        modalImg.setAttribute('src',img.src)
+        captionText.innerHTML = img.alt;
     }
+});
+
+var span = document.querySelector('.close')
+span.onclick = function() {
+  modal.style.display = "none";
 }
